8822173f423b626637929b109ad524490efc8964
Israel D. Gebru <isrish@localhost.localdomain>
2015-06-27 02:26:18 +0200

    changes

--- binuralfeature.py
+++ binuralfeature.py
@@ -1,10 +1,10 @@
 from scipy.io import wavfile
 import os.path
 import numpy as np
-import matplotlib
-from matplotlib import pylab, mlab
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
+from numpy.lib import stride_tricks
 from scikits.samplerate import resample
+from scipy import math
 from fractions import gcd
 
 def extract():
@@ -29,26 +29,85 @@ def readaudio(filename):
 
 """ downsample audio """
 def downsample(snd,oldrate,newrate):
-	r = gcd(newrate,oldrate)
-	new_snd = resample(snd, r,'linear')
-	return new_snd
-
-""" short time fourier transform of audio signal """
-def computestft(samples,samplerate,winsize=2048,overlapFac=0.5,window=np.hanning):
-	s = stft(samples, winsize,overlapFac,window)    
-	return s
-
-def computespectrogram(samples,samplerate,nfft=2048,noverlap=0,window=pylab.window_hanning, mode='default'):
-	spec,freqs,ts,im = pylab.specgram(samples, NFFT=nfft, Fs=samplerate, window=window, noverlap=noverlap, cmap =None,xextent=None, pad_to=None, sides='onesided',scale_by_freq=None)
-	return (spec,freqs,ts,im)
+	d = gcd(oldrate,newrate)
+	new_snd   = resample(snd,newrate/oldrate,'sinc_best')
+	return np.array(new_snd)
 
 
-def plotintime(frq,snd,chnum=1):
-	le, ch = snd.shape
+def myspectrogram(x,nfft=2048,frameSize=512,fs=1,window=np.hamming,noverlapratio=0.5):
+	noverlap = int(frameSize*noverlapratio)
+	win = window(frameSize)
+	M =  len(win); # frameSize
+	if len(x)< M: # zero-pad to fill a window
+		x = np.hstack((x,np.zeros((M-len(x),))))
+
+	Modd = np.mod(M,2) # O if M is even, 1 if odd
+	Mo2 = (M-Modd)/2;
+	
+	if noverlap < 0:
+		nhop = -noverlap
+	else:
+		nhop = M-noverlap
+
+	nx = len(x)
+	nframes = int((nx-noverlap)/nhop)
+	X = np.array(np.zeros((nfft,nframes)), dtype=complex) # allocate output spectrogram
+	zp = np.zeros((nfft-M,)) # zero padding each FFT
+	xframe = np.zeros((M,))
+	xoff  = 0 - Mo2 #input time offset = half a frame
+
+	for m in range(0,nframes):
+		if xoff < 0:
+			xframe[0:xoff+M] = x[0:xoff+M,] # partial input data frame
+		else:
+			xframe = x[xoff:xoff+M]
+
+		xw= win * xframe # apply window
+		xwzp = np.hstack((xw[Mo2:M],zp,xw[1:Mo2]))
+		fft_ = np.fft.fft(xwzp,nfft)
+		X[:,m] = fft_
+		xoff += nhop
+
+	return X 
+
+def feature_ILDandIPD(XL_,XR_):
+	ILD_LR = 20*np.log10(abs(XR_)/abs(XL_))
+	IPD_LR = np.angle(XR_/np.conj(XL_))
+	return (ILD_LR,IPD_LR)
+
+
+def plotspectrogram(XL,nfft,fs,title='spectrogram'):	
+	autopower = np.abs(XL * np.conj(XL))
+	result  = 20*np.log10(autopower) 
+	#result = np.clip(result, -40, 200)    # clip values
+
+	x = np.arange(0,XL.shape[1])
+	y = np.arange(0,XL.shape[0])
+	y *= (fs/2)/(nfft/2)
+
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	ax.imshow(result, origin='lower', interpolation='bilinear',cmap='jet', aspect='auto')
+	ax.set_ylabel('Frequency (Hz)')
+	ax.set_title(title)
+	# Tweak spacing to prevent clipping of tick-labels
+	plt.subplots_adjust(bottom=0.15)
+	plt.show()
+
+def plotIPD_ILD(x,type=''):
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	ax.imshow(x, origin='lower', interpolation='bilinear',cmap='jet', aspect='auto')
+	ax.set_ylabel('Frequency (Hz)')
+	ax.set_title(type)
+	plt.show()
+
+def plotintime(frq,snd,chnum=0):
+	le = len(snd)
 	timearray = np.arange(0,le*1.0,1)
 	timearray = timearray/frq
 	timearray = timearray*1000 #scale ot milliseconds
-	plt.plot(timearray,snd[:,chnum],color='b')
+	plt.plot(timearray,snd,color='b')
 	plt.ylabel('Amplitude')
 	plt.xlabel('Time (ms)')
 	plt.xlim([0,le*1000.0/frq])
--- test.py
+++ test.py
@@ -2,28 +2,95 @@ import binuralfeature as bf
 import numpy as np
 import matplotlib.pyplot as plt
 from pylab import *
+from mpl_toolkits.mplot3d import Axes3D
 
 filename = "/scratch/Datasets/AVASM/annotated_white_noise/Recorded/1.wav"
 #filename = "/scratch/Python/BinuralAudio/sf2_cln.wav"
 frq, snd = bf.readaudio(filename)
-le = snd.shape[0]
+print snd.shape
 
 #bf.plotintime(frq,snd,2)
-NFFT =  int(frq*0.02) # 20ms window
-noverlap = int(frq*0.01) #10ms window
-Pxx, freqs, bins, _ = bf.computespectrogram(snd[:,0],frq,nfft=NFFT,noverlap=noverlap, mode='complex')
-plt.xlim([0,le/frq])
-plt.ylim([0,frq/2])
-show()
+snd1 = snd[:,0];
 
-new_frq = 16000
-dnw_snd = bf.downsample(snd[:,0],frq,new_frq)
-print dnw_snd.shape
-le = dnw_snd.shape[0]
+fs = 16000.
+dnw_snd = bf.downsample(snd1,frq,fs)
+print 'New downsampled size:', dnw_snd.shape
+bf.plotintime(fs,dnw_snd,0)
+plt.show()
 
-NFFT =  int(new_frq*0.02) # 20ms window
-noverlap = int(new_frq*0.01) #10ms window
-Pxx, freqs, bins, _ = bf.computespectrogram(dnw_snd,new_frq,nfft=NFFT,noverlap=noverlap, mode='complex')
-plt.xlim([0,le/new_frq])
-plt.ylim([0,new_frq/2])
-show()
+
+# Parameter setting
+time_win = 64.
+frame_shift = 32.
+ns=fs/1000*time_win;
+nfft=1024;
+no=fs/1000*(time_win-frame_shift);
+nzp=ns/2;
+zp=np.zeros((nzp,));
+
+
+XL = bf.myspectrogram(np.hstack((zp,dnw_snd,zp)),nfft,ns,fs,np.hamming,noverlapratio=0.5)
+nfreq,nframe =XL.shape
+nfreq = nfreq/2;
+inside = range(int(np.rint(nzp/(ns-no))),int(np.rint(nframe-nzp/(ns-no))))
+XL = XL[0:nfreq,inside];
+print 'Spectogram Size:', XL.shape
+
+
+# plot Spectogram
+bf.plotspectrogram(XL,nfft,fs,title='CH-Left')
+
+
+
+## RIGHT
+#bf.plotintime(frq,snd,2)
+snd1 = snd[:,1];
+
+dnw_snd = bf.downsample(snd1,frq,fs)
+print 'New downsampled size:', dnw_snd.shape
+bf.plotintime(fs,dnw_snd,0)
+plt.show()
+
+
+# Parameter setting
+time_win = 64.
+frame_shift = 32.
+ns=fs/1000*time_win;
+nfft=1024;
+no=fs/1000*(time_win-frame_shift);
+nzp=ns/2;
+zp=np.zeros((nzp,));
+
+
+XR= bf.myspectrogram(np.hstack((zp,dnw_snd,zp)),nfft,ns,fs,np.hamming,noverlapratio=0.5)
+nfreq,nframe =XR.shape
+nfreq = nfreq/2;
+inside = range(int(np.rint(nzp/(ns-no))),int(np.rint(nframe-nzp/(ns-no))))
+XR = XR[0:nfreq,inside];
+print 'Right Spectogram Size:', XR.shape
+
+
+# plot Spectogram
+bf.plotspectrogram(XR,nfft,fs,title='CH-right')
+
+
+
+
+ILD,IPD = bf.feature_ILDandIPD(XL,XR)
+
+bf.plotIPD_ILD(ILD,'ILD')
+bf.plotIPD_ILD(IPD,'IPD')
+
+
+
+
+
+
+
+
+
+
+
+''' max freq recoveed is f2/2 ==8000hz, using nfft=1024 point the frequency 
+resolution is fs/2/nfft=7.8hz per frequency bin,
+'''
